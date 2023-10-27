98fe229c7dfca741ba6dc17e0a9ab70066cfddee
iMaeGoo <mail1st@qq.com>
2023-10-27 09:07:39 +0800

    Add console log

--- .zeabur/output/functions/index.func/index.js
+++ .zeabur/output/functions/index.func/index.js
@@ -1,6 +1,9 @@
 module.exports = (req, res) => {
   const { name = "friend" } = req.query;
 
+  console.log('Incoming request');
+  console.log(req);
+
   const text =
     `Howdy ${name}, from Zeabur!\n` +
     `Node.js: ${process.version}\n` +
